44c1daba3b2be0f49a94b09bcbb06c4ab7beb756
per1Peteia <justusmanthey@googlemail.com>
2025-06-23 07:58:47 +0200

    CI: sec check

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,15 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginJs from "@eslint/js";
+import pluginSecurity from "eslint-plugin-security";
 
-
+/** @type {import('eslint').Linter.Config[]} */
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
+	{ files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+	{ files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginSecurity.configs.recommended,
 ]);
+
